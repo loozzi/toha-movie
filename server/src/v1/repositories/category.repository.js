8b11aacb682e7a3e7eeb5b799c550c5717cf1967
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-01-14 16:02:10 +0700

    add route create/update/delete category

--- server/src/v1/repositories/category.repository.js
+++ server/src/v1/repositories/category.repository.js
@@ -19,6 +19,10 @@ const findOneBySlug = async (slug) => {
 	return (await query(`select * from categories where slug = '${slug}' and is_deleted = false`))[0]
 }
 
+const findOneById = async (id) => {
+	return (await query(`select * from categories where id = ${id} and is_deleted = false`))[0]
+}
+
 const create = async ({ name, slug }) => {
 	try {
 		await query(`insert into categories set ?`, { name, slug })
@@ -53,22 +57,49 @@ const isMovieExist = async ({ movie_id, category_id }) => {
 	return (await query(`select * from movies_categories where movie_id = ${movie_id} and category_id = ${category_id} and is_deleted = false`)).length > 0
 }
 
-const all = async () => {
+const all = async ({ limit, offset }) => {
 	return await query(`
 		select c.id, c.name, c.slug, count(mc.movie_id) as count, c.modified
 		from categories c 
 		left join movies_categories mc on c.id = mc.category_id
 		where c.is_deleted = false
-		group by c.id;
+		group by c.id
+		limit ${limit} offset ${offset};
 	`)
+}
+
+const update = async ({ id, name, slug }) => {
+	try {
+		await query(`update categories set ? where id = ${id}`, { name, slug })
+		return true
+	} catch (err) {
+		return false
+	}
+}
+
+const remove = async ({ id }) => {
+	try {
+		await query(`update categories set is_deleted = true where id = ${id}`)
+		return true
+	} catch (err) {
+		return false
+	}
+}
+
+const count = async () => {
+	return (await query(`select count(*) as count from categories where is_deleted = false`))[0].count
 }
 
 module.exports = {
 	findByMovieId,
 	findOneBySlug,
+	findOneById,
 	create,
 	addMovie,
 	removeMovie,
 	isMovieExist,
-	all
+	all,
+	update,
+	remove,
+	count
 }
--- server/src/v1/services/category.service.js
+++ server/src/v1/services/category.service.js
@@ -1,16 +1,80 @@
 const categoryRepo = require('../repositories/category.repository')
+const paginationService = require('../utils/pagination.service')
 
-const all = async () => {
-	const categories = await categoryRepo.all()
+const all = async ({ current_page, limit_page }) => {
+	const limit = limit_page
+	const offset = (current_page - 1) * limit
+
+	const totalItem = await categoryRepo.count()
+
+	const categories = await categoryRepo.all({ limit, offset })
+
 	return {
 		status: 200,
 		message: 'Success',
-		elements: {
-			categories
+		elements: paginationService.to_form({
+			current_page,
+			limit,
+			total_item: totalItem,
+			data: categories
+		})
+	}
+}
+
+const create = async ({ name, slug }) => {
+	const catOld = await categoryRepo.findOneBySlug(slug)
+	if (catOld) {
+		return {
+			status: 400,
+			message: 'Category is exist'
 		}
+	}
+
+	const isCreated = await categoryRepo.create({ name, slug })
+
+	return {
+		status: isCreated ? 200 : 500,
+		message: isCreated ? 'Success' : 'Failed'
+	}
+}
+
+const update = async ({ id, name, slug }) => {
+	const isExist = await categoryRepo.findOneById(id)
+	if (!isExist) {
+		return {
+			status: 400,
+			message: 'Category is not exist'
+		}
+	}
+
+	const isUpdated = await categoryRepo.update({ id, name, slug })
+
+	return {
+		status: isUpdated ? 200 : 500,
+		message: isUpdated ? 'Success' : 'Failed'
+	}
+}
+
+const remove = async ({ id }) => {
+	const isExist = await categoryRepo.findOneById(id)
+	if (!isExist) {
+		return {
+			status: 400,
+			message: 'Category is not exist'
+		}
+	}
+
+	const isDeleted = await categoryRepo.remove({ id })
+
+	return {
+		status: isDeleted ? 200 : 500,
+		message: isDeleted ? 'Success' : 'Failed'
 	}
 }
 
 module.exports = {
-	all
+	all,
+	create,
+	update,
+	delete: remove
 }
