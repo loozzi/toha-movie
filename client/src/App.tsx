bbe604a3d6f9a376336aa1b3830c25408cbb2e14
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-01-21 17:21:41 +0700

    add register page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import routesConfig from '~/configs/routes.config'
 import PublicLayout from '~/pages/public'
 import LoginPage from '~/pages/auth/login'
 import { ConfigProvider, theme } from 'antd'
+import RegisterPage from './pages/auth/register'
 
 function App() {
   const location = useLocation()
@@ -13,6 +14,7 @@ function App() {
       <Routes location={location}>
         <Route path={routesConfig.public} element={<PublicLayout />}>
           <Route path={routesConfig.auth.login} element={<LoginPage />} />
+          <Route path={routesConfig.auth.register} element={<RegisterPage />} />
         </Route>
       </Routes>
     </ConfigProvider>
