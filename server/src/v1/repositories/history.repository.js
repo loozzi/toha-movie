01c05f34a2e6cb70f5f69afe2e22bba612092d02
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-01-29 17:12:56 +0700

    update: server_id, episode_name of history & update img_url of actor

--- server/src/v1/repositories/history.repository.js
+++ server/src/v1/repositories/history.repository.js
@@ -2,7 +2,8 @@ const query = require('../../config/query')
 
 const all = async ({ user_id, limit, offset }) => {
 	return await query(
-		`SELECT * FROM histories 
+		`SELECT movie_id, cur_time, episode_name, server_id, modified
+		FROM histories 
 		WHERE user_id = ${user_id} and is_deleted = FALSE
 		ORDER BY modified DESC
 		LIMIT ${limit} OFFSET ${offset};`
@@ -19,19 +20,23 @@ const count = async ({ user_id }) => {
 const findOne = async ({ user_id, movie_id }) => {
 	const histories = await query(
 		`SELECT * FROM histories 
-		WHERE user_id = ${user_id} and movie_id = ${movie_id} and is_deleted = FALSE`
+		WHERE user_id = ${user_id} and movie_id = ${movie_id}`
 	)
 	return histories.length > 0 ? histories[0] : null
 }
 
-const update = async ({ user_id, movie_id }) => {
+const update = async ({ user_id, movie_id, server_id, cur_time, episode_name }) => {
 	await query(
-		`UPDATE histories SET modified = now() 
+		`UPDATE histories SET modified = now(), 
+			cur_time = ${cur_time}, 
+			episode_name = '${episode_name}', 
+			server_id = ${server_id},
+			is_deleted = FALSE
 		WHERE user_id = ${user_id} and movie_id = ${movie_id}`
 	)
 }
 
-const create = async ({ user_id, movie_id }) => {
+const create = async ({ user_id, movie_id, server_id, episode_name }) => {
 	try {
 		const isCreated = await findOne({ user_id, movie_id })
 		if (isCreated) {
@@ -39,7 +44,8 @@ const create = async ({ user_id, movie_id }) => {
 			return true
 		}
 		await query(
-			`INSERT INTO histories (user_id, movie_id, cur_time) VALUES (${user_id}, ${movie_id}, 0)`
+			`INSERT INTO histories (user_id, movie_id, cur_time, server_id, episode_name) 
+			VALUES (${user_id}, ${movie_id}, 0, ${server_id}, '${episode_name}', ${episode_name})`
 		)
 		return true
 	} catch (err) {
